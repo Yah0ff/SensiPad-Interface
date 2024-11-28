7798bcded46fc1a1990f0d054a0f00182aaabf8d
Yahoff <gagy020416@gs.utm.mx>
2024-11-28 16:53:51 -0600

    Envio de información por correo

--- V1.py
+++ V1.py
@@ -6,7 +6,20 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from scipy.ndimage import gaussian_filter
 import io
+from tkinter import PhotoImage
 from PIL import Image
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+from email.mime.text import MIMEText
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+#Tamaño de pantalla
+password = os.getenv("PASSWORD")
 
 # Datos simulados de presión plantar
 x = np.linspace(0, 1, 100)
@@ -83,7 +96,62 @@ def generar_pdf():
     # Guardar el archivo PDF
     nombre_archivo = f"Informe_{nombre_var.get()}.pdf"
     pdf.output(nombre_archivo)
-    messagebox.showinfo("Éxito", f"El PDF se ha generado correctamente: {nombre_archivo}")
+
+def enviar_correo():
+    generar_pdf()
+    try:
+        # Dirección del remitente y destinatario
+        remitente = "ggroproy@gmail.com"
+        destinatario = f"{destinatario_var.get()}"
+        
+        # Obtener el nombre del archivo desde la variable de entrada
+        nombre_archivo = f"Informe_{nombre_var.get()}.pdf"  # Aquí tomamos el nombre de la entrada
+        
+        # Crear el mensaje
+        msg = MIMEMultipart()
+        msg['From'] = remitente
+        msg['To'] = destinatario
+        msg['Subject'] = f'Informe de resultados plantares {nombre_var.get()}'
+        
+        cuerpo = f"""<html>
+                <body>
+                    <p>Estimado/a {nombre_var.get()},</p>
+                    <p>Espero que este mensaje le encuentre bien. Adjunto encontrará el informe con los resultados de su evaluación plantar.</p>
+                    <p>Es importante que siga las recomendaciones indicadas en el informe para mejorar su bienestar y evitar posibles complicaciones en el futuro. Si tiene alguna duda o necesita realizar alguna consulta adicional, no dude en contactarme.</p>
+                    <p>Recuerde que la prevención es clave para mantener una buena salud y calidad de vida.</p>
+                    <p>Saludos cordiales,</p>
+                    <p>Dr. Alejandro Rivera<br>Ortopedista</p>
+                    <p><em>"Cuidamos de tu bienestar, para que sigas dando los mejores pasos."</em></p>
+                </body>
+            </html>"""
+        msg.attach(MIMEText(cuerpo, 'html'))
+
+
+        # Ruta del archivo
+        archivo_path = nombre_archivo  # Usamos el nombre definido dinámicamente
+        attachment = open(archivo_path, 'rb')
+
+        part = MIMEBase('application', 'octet-stream')
+        part.set_payload(attachment.read())
+        encoders.encode_base64(part)
+        part.add_header('Content-Disposition', f'attachment; filename= {nombre_archivo}')
+        
+        msg.attach(part)
+        
+        # Conectar con el servidor SMTP de Gmail
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(remitente,password)  # Usa tu contraseña o una contraseña de aplicación si es necesario
+
+        # Enviar el correo
+        server.sendmail(remitente, destinatario, msg.as_string())
+        server.quit()
+        
+        messagebox.showinfo("Éxito", "El archivo se ha enviado correctamente.")
+
+    except Exception as e:
+        messagebox.showerror("Error", f"No se pudo enviar el correo: {e}")
+        print(f"Error detallado: {e}")
 
 
 def mostrar_informacion():
@@ -116,12 +184,12 @@ def iniciar_sesion():
         messagebox.showerror("Error", "Usuario o contraseña incorrectos")
 
 def crear_interfaz_principal():
-    global root, nombre_var, edad_var, sexo_var, peso_var, altura_var, diagnostico_var, actividad_var
+    global root, nombre_var, edad_var, sexo_var, peso_var, altura_var, diagnostico_var, actividad_var , destinatario_var
 
     # Crear la ventana principal
     root = tk.Tk()
     root.title("Interfaz de Presión Plantar")
-    root.geometry("800x600")
+    root.geometry("900x900")
     root.protocol("WM_DELETE_WINDOW", cerrar_aplicacion)
 
     # Variables para la información del paciente
@@ -132,39 +200,47 @@ def crear_interfaz_principal():
     altura_var = tk.StringVar()
     diagnostico_var = tk.StringVar()
     actividad_var = tk.StringVar()
+    destinatario_var = tk.StringVar()
 
     # Cuadros de texto y labels organizados
     frame_info = tk.Frame(root)
     frame_info.pack(pady=10)
 
-    # Fila 1 (Nombre)
+    # Fila 1 (Nombre,Edad)
     tk.Label(frame_info, text="Nombre:").grid(row=0, column=0, padx=5, pady=5, sticky="e")
-    tk.Entry(frame_info, textvariable=nombre_var).grid(row=0, column=1, padx=5, pady=5)
+    tk.Entry(frame_info, textvariable=nombre_var, width=30).grid(row=0, column=1, columnspan=3, sticky="ew", padx=5, pady=5)
 
-    # Fila 2 (Edad, Sexo, Peso, Altura)
-    tk.Label(frame_info, text="Edad:").grid(row=1, column=0, padx=5, pady=5, sticky="e")
-    tk.Entry(frame_info, textvariable=edad_var).grid(row=1, column=1, padx=5, pady=5)
 
-    tk.Label(frame_info, text="Sexo:").grid(row=1, column=2, padx=5, pady=5, sticky="e")    
-    tk.Entry(frame_info, textvariable=sexo_var).grid(row=1, column=3, padx=5, pady=5)
+    tk.Label(frame_info, text="Edad:").grid(row=0, column=4, padx=5, pady=5, sticky="e")
+    tk.Entry(frame_info, textvariable=edad_var).grid(row=0, column=5, padx=5, pady=5)
 
-    tk.Label(frame_info, text="Peso:").grid(row=1, column=4, padx=5, pady=5, sticky="e")
-    tk.Entry(frame_info, textvariable=peso_var).grid(row=1, column=5, padx=5, pady=5)
+   # Fila 2 (Sexo, Peso, Altura)
+    tk.Label(frame_info, text="Sexo:").grid(row=1, column=0, padx=5, pady=5, sticky="e")    
+    tk.Entry(frame_info, textvariable=sexo_var).grid(row=1, column=1, padx=5, pady=5)
 
-    tk.Label(frame_info, text="Altura:").grid(row=1, column=6, padx=5, pady=5, sticky="e")
-    tk.Entry(frame_info, textvariable=altura_var).grid(row=1, column=7, padx=5, pady=5)
+    tk.Label(frame_info, text="Peso:").grid(row=1, column=2, padx=5, pady=5, sticky="e")
+    tk.Entry(frame_info, textvariable=peso_var).grid(row=1, column=3, padx=5, pady=5)
+
+    tk.Label(frame_info, text="Altura:").grid(row=1, column=4, padx=5, pady=5, sticky="e")
+    tk.Entry(frame_info, textvariable=altura_var).grid(row=1, column=5, padx=5, pady=5)
 
     # Fila 3 (Actividad física)
     tk.Label(frame_info, text="Actividad física:").grid(row=2, column=0, padx=5, pady=5, sticky="e")
-    tk.Entry(frame_info, textvariable=actividad_var).grid(row=2, column=1, columnspan=3, padx=5, pady=5)
+    tk.Entry(frame_info, textvariable=actividad_var).grid(row=2, column=1, columnspan=5, sticky="ew",padx=5, pady=5)
 
     # Fila 4 (Diagnóstico previo)
     tk.Label(frame_info, text="Diagnóstico previo:").grid(row=3, column=0, padx=5, pady=5, sticky="e")
-    tk.Entry(frame_info, textvariable=diagnostico_var).grid(row=3, column=1, columnspan=3, padx=5, pady=5)
+    tk.Entry(frame_info, textvariable=diagnostico_var).grid(row=3, column=1, columnspan=5, sticky="ew",padx=5, pady=5)
+
+    # Fila 5 (Correo)
+    tk.Label(frame_info, text="Correo:").grid(row=4, column=0, padx=5, pady=5, sticky="e")
+    tk.Entry(frame_info, textvariable=destinatario_var).grid(row=4, column=1, columnspan=5, sticky="ew",padx=5, pady=5)
 
     # Botones
-    tk.Button(root, text="Generar PDF", command=generar_pdf).pack(pady=10)
-    tk.Button(root, text="Mostrar Información", command=mostrar_informacion).pack(pady=10)
+    tk.Button(frame_info, text="Generar PDF", command=generar_pdf).grid(row=5, column=1, padx=5, pady=5, sticky="e")
+    tk.Button(frame_info, text="Mostrar Información", command=mostrar_informacion).grid(row=5, column=2, padx=5, pady=5, sticky="e")
+    tk.Button(frame_info, text="Correo", command=enviar_correo).grid(row=5, column=3, padx=5, pady=5, sticky="e")
+
 
     fig, axes = plt.subplots(1, 2, figsize=(10, 5), gridspec_kw={'width_ratios': [1, 1.1]})
 # Nota: el ajuste en `width_ratios` permite compensar el espacio de la barra de color.
@@ -208,5 +284,7 @@ tk.Label(ventana_inicio, text="Contraseña:").pack(pady=5)
 tk.Entry(ventana_inicio, textvariable=contrasena_var, show="*").pack(pady=5)
 
 tk.Button(ventana_inicio, text="Iniciar sesión", command=iniciar_sesion).pack(pady=10)
+
+ventana_inicio.iconphoto(True,PhotoImage(file="Logo.png"))
 
 ventana_inicio.mainloop()
